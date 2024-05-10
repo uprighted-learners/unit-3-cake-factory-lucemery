aa362f54edfea909736b78f5611914d30f3a8385
lucemery <lucemery316@gmail.com>
2024-05-10 17:02:31 -0500

    not done

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 class Cake {
     // your code here
+    constructor(flavor, icing, decoration) {
+        this.flavor = flavor;
+        this.icing = icing;
+        this.decoration = decoration;
+    }
+    
+    let birthdayCake = new Cake
+    birthdayCake.describe() {
+        console.log("A delicious vanilla cake, with mint icing, and a candle");
+    }
 }
+
+const strawberryCake = new Cake(""
+)
+
+
+
 
 let chocolateCake = new Cake("chocolate", "caramel", "sprinkles");
 
